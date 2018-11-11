1b442f2a5cdfcf1e32cdb91b1bd8b12907f9cf4b
SaxarKompot <saxarkompot@gmail.com>
2018-11-11 22:48:29 +0200

    add operator support

---
+++ Calculator/index.js
@@ -0,0 +1,37 @@
+var box;
+var result = document.getElementById("result");
+function clickButton() {
+    result.value += this.innerText;
+}
+var arrayDigit = document.getElementsByClassName("digit");
+for (var i = 0; i < arrayDigit.length; i++) {
+    arrayDigit[i].onclick = clickButton;
+}
+
+
+var plus = document.getElementById("sum");
+var equalBtn = document.getElementById("equal");
+var clickedOperator;
+
+var equal = function () {
+    if (clickedOperator == "+")
+        result.value = parseInt(box) + parseInt(result.value);
+    if (clickedOperator == "-")
+        result.value = parseInt(box) - parseInt(result.value);
+    if (clickedOperator == "x")
+        result.value = parseInt(box) * parseInt(result.value);
+}
+equalBtn.onclick = equal;
+
+
+var operation = function () {
+    box = result.value;
+    result.value = "";
+    clickedOperator = this.innerText;
+}
+
+var arrayOperator = document.getElementsByClassName("operator");
+for (var i = 0; i < arrayOperator.length; i++) {
+    arrayOperator[i].onclick = operation;
+}
+
