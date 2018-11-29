6b6b7dd0681721edba96c75891b19d4bbf070505
chapluck <chapluck@gmail.com>
2018-11-29 11:01:27 +0200

    Add list of objects to make simple tasks

---
+++ SimpleTasks/objects.js
@@ -0,0 +1,55 @@
+var persons = [{
+    name: "Vasily",
+    lastname: "Pistol",
+    sex: "M",
+    age: 78,
+},
+{
+    name: "Svetlana",
+    lastname: "Pistol",
+    sex: "F",
+    age: 77,
+},
+{
+    name: "Igor",
+    lastname: "Girshman",
+    sex: "M",
+    age: 14,
+}, {
+    name: "Sebastian",
+    lastname: "Balzak",
+    sex: "M",
+    age: 38,
+}, {
+    name: "Viktoria",
+    lastname: "Garbage",
+    sex: "F",
+    age: 27,
+}, {
+    name: "Vasily",
+    lastname: "Garbage",
+    sex: "M",
+    age: 11,
+},
+{
+    name: "Ilya",
+    lastname: "Black",
+    sex: "M",
+    age: 21,
+}, {
+    name: "John",
+    lastname: "Smith",
+    sex: "M",
+    age: 78,
+}, {
+    name: "Julia",
+    lastname: "Smith",
+    sex: "F",
+    age: 78,
+}, {
+    name: "Vasily",
+    lastname: "Reebok",
+    sex: "M",
+    age: 45,
+},
+]
